128f5aae0c3c6fee54a3a19e6968478ea15a5ad8
hmelica <hippolytemelica@icloud.com>
2023-10-14 19:35:43 +0200

    var expansion ready to test (NOT TESTER YET)

---
+++ test/t_expansion.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_expansion.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmelica <hmelica@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 21:57:09 by hmelica           #+#    #+#             */
+/*   Updated: 2023/10/01 16:28:11 by hmelica          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minishell.h"
+#include "include/criterion/criterion.h"
+#include "include/criterion/parameterized.h"
+#include "include/criterion/new/assert.h"
+#include <string.h>
+
+// test var expansion there
