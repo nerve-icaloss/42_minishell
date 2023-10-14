e0e61d954f46a0ea1539422ae4ac9323f4c412cd
hmelica <hippolytemelica@icloud.com>
2023-10-14 17:21:28 +0200

    strjoin2 update (works if one of the string is NULL) and some other fixes

--- src/libft/src/ft_str/ft_strjoin2.c
+++ src/libft/src/ft_str/ft_strjoin2.c
@@ -17,26 +17,20 @@ char	*ft_strjoin2(char *s1, char *s2, int free_s1, int free_s2)
 	char	*ret;
 	int		i;
 
+	if (!s1 && !s2)
+		return (NULL);
 	ret = malloc(sizeof(*ret) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!ret || !s1 || !s2)
+	if (!ret)
 		return (NULL);
 	i = 0;
-	while (*s1)
-	{
-		ret[i] = *s1;
-		s1++;
-		i++;
-	}
-	while (*s2)
-	{
-		ret[i] = *s2;
-		i++;
-		s2++;
-	}
+	while (s1 && *s1)
+		ret[i++] = *(s1++);
+	while (s2 && *s2)
+		ret[i++] = *(s2++);
 	ret[i] = '\0';
-	if (free_s1 == 1)
+	if (s1 && free_s1)
 		free(s1);
-	if (free_s2 == 1)
+	if (s2 && free_s2)
 		free(s2);
 	return (ret);
 }
