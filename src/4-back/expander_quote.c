e9e85a768429610893cf9cb53bdc24d14b2afdb9
hmelica <hippolytemelica@icloud.com>
2023-10-25 20:21:53 +0200

    remove quotes now without malloc

--- src/4-back/expander_quote.c
+++ src/4-back/expander_quote.c
@@ -6,32 +6,33 @@
 /*   By: nserve <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 18:55:54 by nserve            #+#    #+#             */
-/*   Updated: 2023/10/25 10:01:46 by hmelica          ###   ########.fr       */
+/*   Updated: 2023/10/25 20:21:39 by hmelica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static int	handle_quotes(char **ret, char *src, int *i, int *j)
+static int	handle_quotes(char *src, int *i, int *j)
 {
 	static const char	quotes[3] = "'\"";
 	int					k;
+	char				*first;
 
-	if (!ret || !src || !i || !j)
+	if (!src || !i || !j)
 		return (1);
 	while (src[*i + *j] && !strchr(quotes, src[*i + *j]))
 		(*j)++;
-	*ret = ft_strjoin2(*ret, ft_substr(src, *i, *j), 1, 1);
-	if (!src[*i + *j])
-		return (1);
+	first = src + *i + *j;
 	k = strchr(quotes, src[*i + *j]) - quotes;
 	*i += *j + 1;
 	*j = 0;
 	while (src[*i + *j] && src[*i + *j] != quotes[k])
 		(*j)++;
-	*ret = ft_strjoin2(*ret, ft_substr(src, *i, *j), 1, 1);
-	if (!src[*i + *j])
-		return (1);
+	if (src[*i + *j])
+	{
+		ft_offset(src + *i + *j, 1);
+		ft_offset(first, 1);
+	}
 	*i += *j + 1;
 	*j = 0;
 	return (0);
@@ -53,11 +54,8 @@ static int	remove_quotes_word(t_node *word)
 	i = 0;
 	j = 0;
 	while (word->val && word->val[i + j])
-		if (handle_quotes(&ret, word->val, &i, &j))
+		if (handle_quotes(word->val, &i, &j))
 			break ;
-	if (word->val)
-		free(word->val);
-	word->val = ret;
 	return (0);
 }
 
@@ -86,10 +84,7 @@ int	remove_quotes_str(char **str)
 	i = 0;
 	j = 0;
 	while (*str && *str[i + j])
-		if (handle_quotes(&ret, *str, &i, &j))
+		if (handle_quotes(*str, &i, &j))
 			break ;
-	if (*str)
-		free(*str);
-	*str = ret;
 	return (0);
 }
