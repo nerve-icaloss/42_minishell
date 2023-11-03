f84938a1d78badc78260bc35e41a57e110a65097
hmelica <hippolytemelica@icloud.com>
2023-11-03 11:18:13 +0100

    remove quotes word cleaner

--- src/4-back/expander_quote.c
+++ src/4-back/expander_quote.c
@@ -6,7 +6,7 @@
 /*   By: nserve <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 18:55:54 by nserve            #+#    #+#             */
-/*   Updated: 2023/10/29 13:06:51 by hmelica          ###   ########.fr       */
+/*   Updated: 2023/11/03 11:17:44 by hmelica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,6 @@
  * k == 0 means '
  * k == 1 means "
  * */
-static int	remove_quotes_word(t_node *word)
-{
-	static const char	quotes[3] = "'\"";
-	char				*i;
-	char				*j;
-
-	if (!word || !word->val)
-		return (errno = ENODATA, 1);
-	i = word->val;
-	while (i && *i)
-	{
-		while (*i && !ft_strchr(quotes, *i))
-			i++;
-		if (!*i)
-			return (0);
-		j = ft_strchr(i + 1, *i);
-		if (!j)
-			return (0);
-		ft_offset(j, 1);
-		ft_offset(i, 1);
-		i = j - 1;
-	}
-	return (0);
-}
-
-int	remove_quotes(t_node *word)
-{
-	if (!word)
-		return (errno = ENODATA, 1);
-	while (word)
-	{
-		if (remove_quotes_word(word))
-			return (1);
-		word = word->next_sibling;
-	}
-	return (0);
-}
-
 int	remove_quotes_str(char **str)
 {
 	static const char	quotes[3] = "'\"";
@@ -75,6 +37,26 @@ int	remove_quotes_str(char **str)
 		ft_offset(j, 1);
 		ft_offset(i, 1);
 		i = j - 1;
+	}
+	return (0);
+}
+
+static int	remove_quotes_word(t_node *word)
+{
+	if (!word)
+		return (errno = ENODATA, -1);
+	return (remove_quotes_str(&word->val));
+}
+
+int	remove_quotes(t_node *word)
+{
+	if (!word)
+		return (errno = ENODATA, 1);
+	while (word)
+	{
+		if (remove_quotes_word(word))
+			return (1);
+		word = word->next_sibling;
 	}
 	return (0);
 }
